bccead69cce1334bb4a2a0cb545d4ac17fd5577c
CoffeeUai <CoffeeUai@users.noreply.github.com>
2019-10-14 21:36:50 -0300

    Menu-Seletor de niveis

--- Player/KinematicBody2D.gd
+++ Player/KinematicBody2D.gd
@@ -7,9 +7,9 @@ func _physics_process(delta):
 	motion.y += 10
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = 100
+		motion.x = 200
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -100
+		motion.x = -200
 	else:
 		motion.x = 0
 
